04869e83ce4622ea827d800fa40bc4c3e6df985e
hero <hero@DESKTOP-VI3U90H>
2022-05-30 18:27:06 +0430

    initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  toggleShow = true;
-  logArr: string[] = [];
-  count = 1;
-  togglePara() {
-    this.toggleShow = !this.toggleShow;
-    this.addLog();
-  }
-  addLog() {
-    this.logArr.push('Love you' + this.count);
-    this.count++;
-  }
-  getColor(id) {
-    return id > 5 ? true : false;
-  }
-  getColor1(id) {
-    return id > 5 ? 'red': 'transparent';
-  }
+  
 }
