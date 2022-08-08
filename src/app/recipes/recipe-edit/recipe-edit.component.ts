96c2cab3cffc5777a37d7112dc975d2eda6c8075
hero <h_mcamin@yahoo.com>
2022-08-08 15:26:40 +0430

    initialize recipe edit form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Params} from "@angular/router";
+import {FormArray, FormControl, FormGroup} from "@angular/forms";
+import {Recipe} from "../recipe.model";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -8,16 +11,42 @@ import {ActivatedRoute, Params} from "@angular/router";
 export class RecipeEditComponent implements OnInit{
   text: string;
   id: number;
-  constructor(private activeRoute: ActivatedRoute) {
+  form: FormGroup;
+  recipe: Recipe;
+  editMode: boolean;
+  constructor(private activeRoute: ActivatedRoute, private recipeService: RecipeService) {
   }
   ngOnInit() {
     this.activeRoute.params.subscribe((param: Params) => {
       this.id = param['id'];
-      if(this.id){
-        this.text = "we have an ID"
-      } else{
-        this.text = "New Recipe is going to be added"
-      }
+      this.editMode = this.id != null;
+      this.initForm();
     })
+  }
+  initForm(){
+    if(this.editMode){
+      this.recipe = this.recipeService.getRecipesAt(this.id);
+      this.form = new FormGroup({
+        'name': new FormControl(this.recipe.name) ,
+        'imageUrl': new FormControl(this.recipe.imagePath) ,
+        'description': new FormControl(this.recipe.description) ,
+        'ingredients': new FormArray([])
+      })
+      for(let ingredient of this.recipe.ingredients) {
+
+        const control = new FormGroup({
+          'name': new FormControl(ingredient.name),
+          'amount': new FormControl(ingredient.amount)
+        });
+        (<FormArray>this.form.get('ingredients')).push(control);
+      }
+    } else{
+      this.form = new FormGroup({
+        'name': new FormControl(null) ,
+        'imageUrl': new FormControl(null) ,
+        'description': new FormControl(null) ,
+        'ingredients': new FormArray([])
+      })
+    }
   }
 }
