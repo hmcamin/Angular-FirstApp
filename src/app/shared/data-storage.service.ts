aab1bf5ced8be9bf1a81c3fed2a055b847b0deb6
hero <h_mcamin@yahoo.com>
2022-08-10 18:51:05 +0430

    setup store data Http request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,16 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {RecipeService} from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipe: RecipeService) {
+  constructor(private http: HttpClient) {
+  }
+  saveData(){
+    return this.http.get('url').subscribe(data =>{
+      console.log(data);
+    }, error =>{
+      console.log(error);
+    });
   }
 
 }
