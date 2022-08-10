0c1358191b20609566f7119a94659dfa5821cff5
hero <h_mcamin@yahoo.com>
2022-08-10 19:27:52 +0430

    check storage service for data before resolving data from server

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -3,12 +3,19 @@ import {Recipe} from "./recipe.model";
 import {Observable} from "rxjs";
 import {DataStorageService} from "../shared/data-storage.service";
 import {Injectable} from "@angular/core";
+import {RecipeService} from "./recipe.service";
 
 @Injectable({providedIn: "root"})
 export class RecipesResolverService implements Resolve<Recipe[]>{
-  constructor(private storage:DataStorageService) {
+  constructor(private storage:DataStorageService, private recipe: RecipeService) {
   }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
-    return this.storage.fetchData();
+    const recipes = this.recipe.getRecipes();
+
+    if(recipes.length === 0 ){
+      return this.storage.fetchData();
+    } else {
+      return recipes;
+    }
   }
 }
