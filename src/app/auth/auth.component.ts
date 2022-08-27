7e2dcdeed109d006f82e73cc5889da75946b26ce
hero <h_mcamin@yahoo.com>
2022-08-27 23:48:58 +0430

    send signup request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core";
 import {NgForm} from "@angular/forms";
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: "app-auth",
@@ -7,11 +8,26 @@ import {NgForm} from "@angular/forms";
 })
 export class authComponent {
   isLoginMode = true;
+  constructor(private authService: AuthService) {
 
+  }
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode;
   }
-  onSubmit(form: NgForm){
-    form.resetForm();
+  onSubmit(form: NgForm) {
+    if(!form.value){
+      return;
+    }
+    if(this.isLoginMode){
+      // ....
+    } else {
+      this.authService.singUp(form.value.email, form.value.password).subscribe(resData => {
+        console.log(resData);
+      }, error => {
+        console.log(error);
+      });
+      form.resetForm();
+    }
+
   }
 }
