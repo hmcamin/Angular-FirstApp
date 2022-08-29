bc12c406f3bad594cb2fcc2ecc521829e554edb4
hero <h_mcamin@yahoo.com>
2022-08-29 23:26:23 +0430

    login error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {throwError} from "rxjs";
 import {catchError} from "rxjs/operators";
 
@@ -9,19 +9,21 @@ export class AuthService {
   }
   singUp(email: string, password: string){
     return this.http.post<string>('signupLink',{email: email, password: password})
-      .pipe(catchError(errorRes => {
-        let errorMessage = "and error occurred!";
-        if(errorRes.error || errorRes.error.error){
-          return throwError(errorMessage);
-        }
-        switch (errorRes.error.error.message){
-          case 'EMAIL_EXISTS':
-            errorRes = 'Email exists!';
-        }
-        return throwError(errorMessage);
-    }));
+      .pipe(catchError(this.handleError));
   }
   login(email: string, password: string) {
-    return this.http.post<string>('loginRequestLink',{email: email, password: password});
+    return this.http.post<string>('loginRequestLink',{email: email, password: password})
+      .pipe(catchError(this.handleError));
+  }
+  private handleError(errorRes: HttpErrorResponse){
+    let errorMessage = "and error occurred!";
+    if(errorRes.error || errorRes.error.error){
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message){
+      case 'EMAIL_EXISTS':
+        errorMessage = 'Email exists!';
+    }
+    return throwError(errorMessage);
   }
 }
