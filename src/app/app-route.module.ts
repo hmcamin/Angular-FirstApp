f601a0cbbbb7a12014fc41c78495a8311509840c
hero <h_mcamin@yahoo.com>
2022-09-02 01:46:52 +0430

    adding core and auth feature modules

--- src/app/app-route.module.ts
+++ src/app/app-route.module.ts
@@ -1,9 +1,7 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {AuthComponent} from "./auth/auth.component";
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipe', pathMatch: 'full' },
-  { path: 'auth', component: AuthComponent},
   { path: '**', redirectTo: ''}
 ]
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {AppRouteModule} from "./app-route.module";
-import {RecipeService} from "./recipes/recipe.service";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AuthComponent} from "./auth/auth.component";
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
+import {HttpClientModule} from "@angular/common/http";
 import {RecipesModule} from "./recipes/recipes.module";
-import {AuthService} from "./auth/auth.service";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
 import {RecipesRoutingModule} from "./recipes/recipes-routing.module";
+import {CoreModule} from "./core.module";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     AppRouteModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-    RecipesRoutingModule
+    RecipesRoutingModule,
+    CoreModule,
+    AuthModule
   ],
-  providers: [ShoppingListService,AuthService,RecipeService,{
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true
-  }],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
